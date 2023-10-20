9599daf353064fda0f463bc197a1469e795910bc
HabibMollah <habib.prodev@gmail.com>
2023-10-20 18:09:08 +0600

    refactor

--- src/components/SpaceflightCard.tsx
+++ src/components/SpaceflightCard.tsx
@@ -8,15 +8,12 @@ export default function SpaceflightCard({ spaceFlight }: Props) {
   return (
     <li className="list-group-item">
       <p>{spaceFlight.mission_name}</p>
-      {spaceFlight.launch_success ? (
-        <span className="bg-success text-white p-1 rounded-pill fs-6">
-          Success
-        </span>
-      ) : (
-        <span className="bg-danger text-white p-1 rounded-pill fs-6">
-          Failure
-        </span>
-      )}
+      <span
+        className={`text-white p-1 rounded-pill fs-6 ${
+          spaceFlight.launch_success ? 'bg-success' : 'bg-danger'
+        }`}>
+        {spaceFlight.launch_success ? 'Success' : 'Failure'}
+      </span>
     </li>
   );
 }
