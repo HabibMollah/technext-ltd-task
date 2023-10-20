b28a39f4e4a509caf02a94bda8052773f16d512d
HabibMollah <habib.prodev@gmail.com>
2023-10-20 06:53:04 +0600

    refactor and update form

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,29 +1,25 @@
+import Checkbox from './Checkbox';
 import Search from './Search';
+import Select from './Select';
 
 export default function Form() {
   return (
     <form>
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" id="upcoming" />
-        <label className="form-check-label" htmlFor="upcoming">
-          Show upcoming only
-        </label>
-      </div>
-
+      <Checkbox />
       <Search />
 
-      <div>
-        <select className="form-select" aria-label="Sort By Status">
-          <option>By Launch Status</option>
-          <option>By Something Else</option>
-        </select>
-      </div>
+      <Select label="Filter by Status">
+        <option>By Launch Status</option>
+        <option>Success</option>
+        <option>Failure</option>
+      </Select>
 
       <div>
         <select className="form-select" aria-label="Sort By Time">
           <option>By Launch Date</option>
           <option>Last Week</option>
           <option>Last Month</option>
+          <option>Last Year</option>
         </select>
       </div>
     </form>
