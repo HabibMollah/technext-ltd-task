e425843f3e8d7cce2d1fb07d7e9b64329fc9beea
HabibMollah <habib.prodev@gmail.com>
2023-10-20 07:35:06 +0600

    make data fetching hook type-safe

--- src/hooks/useGetData.ts
+++ src/hooks/useGetData.ts
@@ -2,8 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import baseURL from '../api/baseURL';
 
-export default function useGetData() {
-  return useQuery({
+export default function useGetData<DataType>() {
+  return useQuery<DataType, Error>({
     queryKey: ['launches'],
     queryFn: () => axios.get(baseURL).then((res) => res.data),
   });
