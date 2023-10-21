237aa959429fb53956e13375464c7287be848175
HabibMollah <habib.prodev@gmail.com>
2023-10-21 12:20:09 +0600

    finish styling cards

--- src/components/SpaceflightCard.tsx
+++ src/components/SpaceflightCard.tsx
@@ -10,19 +10,17 @@ export default function SpaceflightCard({ spaceFlight, imgSrc }: Props) {
   return (
     <li className="w-100">
       <img src={imgSrc} alt={`image of ${imgSrc}`} />
-      <p>
+      <p className="mb-2">
         <span>Launch Date:</span> {formatDate(spaceFlight.launch_date_local)}
       </p>
-      <div>
-        <h4>{spaceFlight.mission_name}</h4>
-        <p>{spaceFlight.rocket.rocket_name}</p>
+      <div className="spaceflight">
+        <h4 className="m-0">{spaceFlight.mission_name}</h4>
+        <p className="m-0">{spaceFlight.rocket.rocket_name}</p>
       </div>
-      <div>
-        <p>Launch Status: </p>
+      <div className="status">
+        <p className="mb-1">Launch Status: </p>
         <span
-          className={`status-pill ${
-            spaceFlight.launch_success ? 'bg-success' : 'bg-danger'
-          }`}>
+          className={spaceFlight.launch_success ? 'bg-success' : 'bg-danger'}>
           {spaceFlight.launch_success ? 'Success' : 'Failure'}
         </span>
       </div>
