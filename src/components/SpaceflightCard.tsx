451d94be5b8d7f46a08e8ff31a8fe97541c6f176
HabibMollah <habib.prodev@gmail.com>
2023-10-21 11:02:49 +0600

    add date

--- src/components/SpaceflightCard.tsx
+++ src/components/SpaceflightCard.tsx
@@ -1,4 +1,5 @@
 import { SpaceFlight } from '../types/SpaceFlight';
+import formatDate from '../utils/formatDate';
 
 type Props = {
   spaceFlight: SpaceFlight;
@@ -9,6 +10,7 @@ export default function SpaceflightCard({ spaceFlight, imgSrc }: Props) {
   return (
     <li className="w-100">
       <img src={imgSrc} alt={`image of ${imgSrc}`} />
+      <p>Launch Date: {formatDate(spaceFlight.launch_date_local)}</p>
       <h4>{spaceFlight.mission_name}</h4>
       <span
         className={`status-pill ${
