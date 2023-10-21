9f1cdb4bc3908bd81979f7b844f423b3647a9983
HabibMollah <habib.prodev@gmail.com>
2023-10-21 10:47:19 +0600

    map images

--- src/components/SpaceflightCard.tsx
+++ src/components/SpaceflightCard.tsx
@@ -2,11 +2,13 @@ import { SpaceFlight } from '../types/SpaceFlight';
 
 type Props = {
   spaceFlight: SpaceFlight;
+  imgSrc: string;
 };
 
-export default function SpaceflightCard({ spaceFlight }: Props) {
+export default function SpaceflightCard({ spaceFlight, imgSrc }: Props) {
   return (
     <li className="w-100">
+      <img src={imgSrc} alt={`image of ${imgSrc}`} />
       <h4>{spaceFlight.mission_name}</h4>
       <span
         className={`status-pill ${
--- src/components/SpaceflightList.tsx
+++ src/components/SpaceflightList.tsx
@@ -7,11 +7,27 @@ export default function SpaceflightList() {
 
   console.log(data, isLoading, isError);
 
+  const imgSrcList = [
+    'falcon-sat.png',
+    'demo-sat.png',
+    'traliblazer.png',
+    'rat-sat.png',
+    'razak-sat.png',
+    'f9-test-flight.png',
+    'cots-1.png',
+    'cots-2.png',
+    'crs-1.png',
+  ];
+
   return (
     <ul className="">
       {data?.map((spaceFlight, index) => (
         // There's no unique id for each spaceflight, so I used the index as the key 🙁
-        <SpaceflightCard key={index} spaceFlight={spaceFlight} />
+        <SpaceflightCard
+          key={index}
+          spaceFlight={spaceFlight}
+          imgSrc={imgSrcList[index % imgSrcList.length]}
+        />
       ))}
     </ul>
   );
