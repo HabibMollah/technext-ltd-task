6f9c856f7d98d1a819e7ebdbddf7571c2af194e3
HabibMollah <habib.prodev@gmail.com>
2023-10-22 12:15:20 +0600

    make page numbers dynamic

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import ChevronRight from './icons/ChevronRight';
 import ChevronLeft from './icons/ChevronLeft';
+import ThreeDots from './icons/ThreeDots';
 
 type Props = {
   currentPage: number;
@@ -8,7 +9,21 @@ type Props = {
 };
 
 export default function Pagination({ currentPage, setCurrentPage }: Props) {
-  const pageNumbers = Array.from({ length: 10 }, (_, i) => i + 1);
+  const totalPages = 10;
+  const maxVisiblePages = 5;
+  const halfVisiblePages = Math.floor(maxVisiblePages / 2);
+
+  let startPage = Math.max(1, currentPage - halfVisiblePages);
+  const endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
+
+  if (endPage - startPage + 1 < maxVisiblePages) {
+    startPage = Math.max(1, endPage - maxVisiblePages + 1);
+  }
+
+  const pageNumbers = Array.from(
+    { length: endPage - startPage + 1 },
+    (_, i) => startPage + i
+  );
 
   return (
     <ul className="pagination justify-content-center">
@@ -23,9 +38,26 @@ export default function Pagination({ currentPage, setCurrentPage }: Props) {
         </Link>
       </li>
 
+      {startPage > 1 && (
+        <li className="page-item">
+          <Link className="page-link" onClick={() => setCurrentPage(1)} to="/1">
+            1
+          </Link>
+        </li>
+      )}
+
+      {startPage > 2 && (
+        <li className="page-item disabled">
+          <a className="page-link d-flex h-100 align-items-center" href="#">
+            <ThreeDots />
+          </a>
+        </li>
+      )}
+
       {pageNumbers.map((pageNumber) => (
         <li
-          className={`page-item ${pageNumber === currentPage ? 'active' : ''}`}>
+          className={`page-item ${pageNumber === currentPage ? 'active' : ''}`}
+          key={pageNumber}>
           <Link
             className="page-link"
             onClick={() => setCurrentPage(pageNumber)}
@@ -34,13 +66,36 @@ export default function Pagination({ currentPage, setCurrentPage }: Props) {
           </Link>
         </li>
       ))}
-      <li className={`page-item ${currentPage >= 10 ? 'disabled' : ''}`}>
+
+      {endPage < totalPages - 1 && (
+        <li className="page-item disabled">
+          <a href="#" className="page-link d-flex h-100 align-items-center">
+            <ThreeDots />
+          </a>
+        </li>
+      )}
+
+      {endPage < totalPages && (
+        <li className="page-item">
+          <Link
+            className="page-link"
+            onClick={() => setCurrentPage(totalPages)}
+            to={`/${totalPages}`}>
+            {totalPages}
+          </Link>
+        </li>
+      )}
+
+      <li
+        className={`page-item ${currentPage >= totalPages ? 'disabled' : ''}`}>
         <Link
           className="page-link d-flex h-100 align-items-center"
           onClick={() =>
-            setCurrentPage(currentPage >= 10 ? currentPage : currentPage + 1)
+            setCurrentPage(
+              currentPage >= totalPages ? currentPage : currentPage + 1
+            )
           }
-          to={currentPage >= 10 ? '#' : `/${currentPage + 1}`}>
+          to={currentPage >= totalPages ? '#' : `/${currentPage + 1}`}>
           <ChevronRight />
         </Link>
       </li>
