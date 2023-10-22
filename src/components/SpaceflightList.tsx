6d5e82a0eaab55e0a5309627e47c9ac7db2ef9d7
HabibMollah <habib.prodev@gmail.com>
2023-10-22 15:11:21 +0600

    integrate search

--- src/components/SpaceflightList.tsx
+++ src/components/SpaceflightList.tsx
@@ -5,45 +5,59 @@ import SpaceflightCard from './SpaceflightCard';
 import Spinner from './Spinner';
 import { useParams } from 'react-router-dom';
 import Pagination from './Pagination';
+import useSearchContext from '../hooks/useSearchContext';
+
+const imgSrcList = [
+  'falcon-sat.png',
+  'demo-sat.png',
+  'traliblazer.png',
+  'rat-sat.png',
+  'razak-sat.png',
+  'f9-test-flight.png',
+  'cots-1.png',
+  'cots-2.png',
+  'crs-1.png',
+];
 
 export default function SpaceflightList() {
   const { page } = useParams();
   const [currentPage, setCurrentPage] = useState(parseInt(page || '1'));
   const { data, isLoading, isError } = useGetData<SpaceFlight[]>();
+  const [filteredData, setFilteredData] = useState<SpaceFlight[] | undefined>();
   const [offset, setOffset] = useState(0);
   const [paginatedData, setPaginatedData] = useState<
     SpaceFlight[] | undefined
   >();
+  const { searchValue } = useSearchContext();
 
+  // Filter the data
+  useEffect(() => {
+    if (data && searchValue)
+      setFilteredData(
+        data.filter((spaceFlight) =>
+          spaceFlight.rocket.rocket_name.includes(searchValue)
+        )
+      );
+  }, [data, searchValue]);
+
+  // Set the offset for pagination
   useEffect(() => {
     setOffset((currentPage - 1) * 9);
   }, [currentPage]);
 
+  // Set the visible items in the page
   useEffect(() => {
     if (data) {
-      setPaginatedData(data.slice(offset, offset + 9));
+      if (searchValue && filteredData) {
+        setCurrentPage(1);
+        setPaginatedData(filteredData.slice(offset, offset + 9));
+      } else {
+        setPaginatedData(data.slice(offset, offset + 9));
+      }
     }
-  }, [data, offset]);
+  }, [data, filteredData, offset, searchValue]);
 
-  console.log(currentPage);
-  console.log(offset);
-  console.log(paginatedData);
-  console.log(data);
-
-  console.log(currentPage);
-  console.log((currentPage - 1) * 9);
-
-  const imgSrcList = [
-    'falcon-sat.png',
-    'demo-sat.png',
-    'traliblazer.png',
-    'rat-sat.png',
-    'razak-sat.png',
-    'f9-test-flight.png',
-    'cots-1.png',
-    'cots-2.png',
-    'crs-1.png',
-  ];
+  console.log(filteredData);
 
   if (isError)
     return (
@@ -68,7 +82,11 @@ export default function SpaceflightList() {
         </ul>
 
         <Pagination
-          totalPages={Math.ceil(data.length / 9)}
+          totalPages={
+            searchValue && filteredData
+              ? Math.ceil(filteredData.length / 9)
+              : Math.ceil(data.length / 9)
+          }
           currentPage={currentPage}
           setCurrentPage={setCurrentPage}
         />
